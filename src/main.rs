645f9255800f0410200f1eec57bc15693d158e0d
Alexis Le Provost <alexis.leprovost@outlook.com>
2022-12-06 10:10:42 +0100

    Accept multiple example inputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use advent_of_code_2022::solver::{PuzzlePart, Solver, Solve};
+use advent_of_code_2022::solver::{PuzzlePart, Solve, Solver};
 use clap::Parser;
 use eyre::Result;
 
@@ -8,7 +8,7 @@ use eyre::Result;
 #[command(author, version, about, long_about = None)]
 pub struct Opt {
     #[arg(short, long)]
-    example: bool,
+    example: Option<Option<u16>>,
     #[arg(short, long)]
     puzzle_part: PuzzlePart,
     #[arg(short, long)]
@@ -19,12 +19,14 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let opt = Opt::parse();
-    let file = format!(
-        "./data/{}_day{:0width$}",
-        if opt.example { "example" } else { "input" },
-        opt.day,
-        width = 2
-    );
+
+    let file_prefix = if let Some(example) = opt.example {
+        format!("example{}", example.unwrap_or(1))
+    } else {
+        "input".to_string()
+    };
+    let file = format!("./data/{file_prefix}_day{:0width$}", opt.day, width = 2);
+
     let solver = Solver::from_file(Path::new(&file), opt.day)?;
 
     println!("{}", solver.solve(opt.puzzle_part));
